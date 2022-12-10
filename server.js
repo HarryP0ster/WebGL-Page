27a4d6ff221f8c702bd6ea4ef1dd20023ad20b64
HarryP0ster <garryplakat@gmail.com>
2022-12-10 11:16:49 +0800

    Final

--- server.js
+++ server.js
@@ -2,19 +2,13 @@
 
 const port = 3000;
 
-const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'resources')));
 
 app.get('/', function (req, res) {
-    console.log(LoadJsonFile("resources\\assets\\Toy\\toy.json"));
     res.sendFile(path.join(__dirname, "resources/pages/main.html"));
   });
 
@@ -24,20 +18,3 @@ app.listen(port, function (error) {
     else
       console.log("Error Occurred");
   });
-
-  var LoadJsonFile = function(url) 
-  {
-    fs.readFile(url, 'utf-8', (err, output) =>
-    {
-      if (err)
-      {
-        console.log(err);
-        return;
-      }
-      else
-      {
-        console.log(JSON.parse(output));
-        return;
-      }
-    });
-  }
